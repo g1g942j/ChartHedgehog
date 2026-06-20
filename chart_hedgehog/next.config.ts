7292926b81f2429fe2f2b95068648a55c3cd76f4
Ivan Mikolenko <ivanmikolenko2006@gmail.com>
2026-06-20 18:20:20 +0700

    devops: upd next,config

--- chart_hedgehog/next.config.ts
+++ chart_hedgehog/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' ${apiUrl}; frame-ancestors 'self'; form-action 'self'",
+            value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' ${apiUrl}; frame-ancestors 'self'; form-action 'self'`,
           },
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
