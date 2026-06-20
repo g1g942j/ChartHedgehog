ae072d6ef16d2e8c4e11d64b976b7a078ac47463
Ivan Mikolenko <ivanmikolenko2006@gmail.com>
2026-06-20 16:21:10 +0700

    [devops]:added dockerfiles and docker compose

--- chart_hedgehog/next.config.ts
+++ chart_hedgehog/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080';
+
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-
+  output: 'standalone',
   async headers() {
     return [
       {
